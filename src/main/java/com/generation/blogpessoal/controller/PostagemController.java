bb9ff13057a11b4350a73a62e16fed8a4d5693a6
Edney Paes <Edney19982010@gmail.com>
2023-03-14 15:15:57 -0300

    adicionando classe faltante

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class PostagemController {
 	@Autowired
 	private PostagemRespository postagemRespository;
 	
+	@GetMapping
 	public ResponseEntity<List<Postagem>>gelAll(){
 		return ResponseEntity.ok(postagemRespository.findAll());
 	}
